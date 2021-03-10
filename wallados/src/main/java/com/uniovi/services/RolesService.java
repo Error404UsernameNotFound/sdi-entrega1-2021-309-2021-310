ec4a0aa2d7c7219a65fed3524395575c9c985617
uo264197 <71664251+uo264197@users.noreply.github.com>
2021-03-10 20:08:09 +0100

    Implementada la página principal y actualidos los roles

--- wallados/src/main/java/com/uniovi/services/RolesService.java
+++ wallados/src/main/java/com/uniovi/services/RolesService.java
@@ -1,5 +1,9 @@
 package com.uniovi.services;
 
 public class RolesService {
+	String[] roles = { "ROLE_STANDART", "ROLE_PUBLIC", "ROLE_ADMIN" };
 
+	public String[] getRoles() {
+		return roles;
+	}
 }
