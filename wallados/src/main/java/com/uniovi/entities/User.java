2fee4c3730f09e3aae6918bbaaa03f64671b513f
uo264197 <71664251+uo264197@users.noreply.github.com>
2021-03-10 19:29:46 +0100

    Creada la página inicial

--- wallados/src/main/java/com/uniovi/entities/User.java
+++ wallados/src/main/java/com/uniovi/entities/User.java
@@ -9,7 +9,7 @@ public class User {
 	@GeneratedValue
 	private long id;
 	@Column(unique = true)
-	private String dni;
+	private String email;
 	private String name;
 	private String lastName;
 	private String role;
@@ -19,9 +19,9 @@ public class User {
 	@Transient
 	private String passwordConfirm;
 
-	public User(String dni, String name, String lastName) {
+	public User(String email, String name, String lastName) {
 		super();
-		this.dni = dni;
+		this.email = email;
 		this.name = name;
 		this.lastName = lastName;
 	}
@@ -37,12 +37,12 @@ public class User {
 		this.id = id;
 	}
 
-	public String getDni() {
-		return dni;
+	public String getEmail() {
+		return email;
 	}
 
-	public void setDni(String dni) {
-		this.dni = dni;
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getName() {
